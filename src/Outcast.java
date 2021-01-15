9785414b30453435b1d1618b6260e5bd75147367
Chi-Chu319 <chi0chu319@gmail.com>
2021-01-15 20:21:40 +0800

    Outcast implemented

--- src/Outcast.java
+++ src/Outcast.java
@@ -1,12 +1,27 @@
 public class Outcast {
+    private WordNet w;
+
     // constructor takes a WordNet object
     public Outcast(WordNet wordnet) {
-
+        this.w = wordnet;
     }
 
     // given an array of WordNet nouns, return an outcast
     public String outcast(String[] nouns){
-        return "";
+        String result = "";
+        int d_min = Integer.MAX_VALUE;
+        int d = 0;
+        for(String noun:nouns){
+            for(String n:nouns){
+                d += this.w.distance(noun, n);
+            }
+            if (d < d_min){
+                d_min = d;
+                result = noun;
+            }
+        }
+
+        return result;
     }
 
     // see test client below
